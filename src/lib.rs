832cb6ea88bc9e0d91728a1565bf4595f55f70ec
Umut <umutsahin@protonmail.com>
2023-03-24 19:00:31 +0100

    feat: give a helpful compilation error if no storage formats are selected

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
 #![cfg_attr(doctest, doc = "````no_test")]
 #![doc = include_str!("../README.md")]
 
+#[rustfmt::skip]
+#[cfg(not(any(
+    feature = "bincode",
+    feature = "ini",
+    feature = "json",
+    feature = "ron",
+    feature = "toml",
+    feature = "yaml"
+)))]
+compile_error!(concat!(r#"no storage formats are selected!
+
+    If you're not sure which formats you'll need,
+    you can start with selecting all of them:
+
+    bevy-persistent = { version = ""#, env!("CARGO_PKG_VERSION"), r#"", features = ["all"] }
+
+"#));
+
 pub mod builder;
 pub mod persistent;
 pub mod prelude;
