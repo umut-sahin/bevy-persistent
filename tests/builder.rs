00b53163f846858e4e069138c5dc5c5480b05190
Umut <umutsahin@protonmail.com>
2023-04-09 19:18:46 +0200

    chore: move existing tests to conditionally compiled non wasm modules

--- tests/builder.rs
+++ tests/builder.rs
@@ -1,61 +1,69 @@
 mod common;
 use common::*;
 
-#[test]
-#[cfg(feature = "toml")]
-fn test_builder_build() -> anyhow::Result<()> {
-    let tempdir = tempfile::tempdir()?;
+#[cfg(not(target_family = "wasm"))]
+mod native {
+    use super::*;
 
-    let name = "key bindings";
-    let format = StorageFormat::Toml;
-    let path = tempdir.path().join("key-bindings.toml");
-    let default = KeyBindings::default();
+    #[test]
+    #[cfg(feature = "toml")]
+    fn test_builder_build() -> anyhow::Result<()> {
+        let tempdir = tempfile::tempdir()?;
 
-    assert!(!path.exists());
+        let name = "key bindings";
+        let format = StorageFormat::Toml;
+        let path = tempdir.path().join("key-bindings.toml");
+        let default = KeyBindings::default();
 
-    let resource = Persistent::<KeyBindings>::builder()
-        .name(name)
-        .format(format)
-        .path(&path)
-        .default(default.clone())
-        .build();
+        assert!(!path.exists());
 
-    assert!(path.exists());
+        let resource = Persistent::<KeyBindings>::builder()
+            .name(name)
+            .format(format)
+            .path(&path)
+            .default(default.clone())
+            .build();
 
-    assert_eq!(resource.name(), name);
-    assert_eq!(resource.format(), format);
-    assert_eq!(resource.storage(), &Storage::Filesystem { path });
-    assert_eq!(resource.get(), &default);
+        assert!(path.exists());
 
-    Ok(())
-}
+        assert_eq!(resource.name(), name);
+        assert_eq!(resource.format(), format);
+        assert_eq!(resource.storage(), &Storage::Filesystem { path });
+        assert_eq!(resource.get(), &default);
 
-#[test]
-#[should_panic(expected = "persistent resource name is not set")]
-fn test_builder_no_name() {
-    Persistent::<KeyBindings>::builder().build();
-}
+        Ok(())
+    }
 
-#[test]
-#[should_panic(expected = "persistent resource format is not set")]
-fn test_builder_no_format() {
-    Persistent::<KeyBindings>::builder().name("key bindings").build();
-}
+    #[test]
+    #[should_panic(expected = "persistent resource name is not set")]
+    fn test_builder_no_name() {
+        Persistent::<KeyBindings>::builder().build();
+    }
 
-#[test]
-#[should_panic(expected = "persistent resource path is not set")]
-#[cfg(feature = "toml")]
-fn test_builder_no_path() {
-    Persistent::<KeyBindings>::builder().name("key bindings").format(StorageFormat::Toml).build();
-}
+    #[test]
+    #[should_panic(expected = "persistent resource format is not set")]
+    fn test_builder_no_format() {
+        Persistent::<KeyBindings>::builder().name("key bindings").build();
+    }
 
-#[test]
-#[should_panic(expected = "persistent resource default is not set")]
-#[cfg(feature = "toml")]
-fn test_builder_no_default() {
-    Persistent::<KeyBindings>::builder()
-        .name("key bindings")
-        .format(StorageFormat::Toml)
-        .path("")
-        .build();
+    #[test]
+    #[should_panic(expected = "persistent resource path is not set")]
+    #[cfg(feature = "toml")]
+    fn test_builder_no_path() {
+        Persistent::<KeyBindings>::builder()
+            .name("key bindings")
+            .format(StorageFormat::Toml)
+            .build();
+    }
+
+    #[test]
+    #[should_panic(expected = "persistent resource default is not set")]
+    #[cfg(feature = "toml")]
+    fn test_builder_no_default() {
+        Persistent::<KeyBindings>::builder()
+            .name("key bindings")
+            .format(StorageFormat::Toml)
+            .path("")
+            .build();
+    }
 }
